9f3286f38f6b771dda054fffcd6216105b5b9011
Djobbo "Alfie" Maïga <djobbo.maiga@gmail.com>
2020-05-02 03:51:57 +0200

    clean

--- systeme.c
+++ systeme.c
@@ -1,19 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
-// fork()
-#include <unistd.h>
-// waitpid()
-#include <sys/wait.h>
-// pid type
-#include <sys/types.h>
-// time pour rand()
-#include <time.h>
-// Pour la zone de memoire partagee
-#include <sys/shm.h>
-// pour utiliser les semaphores
-#include <sys/sem.h>
-// Pour envoyer des signaux
-#include <signal.h>
+#include <unistd.h>    // fork()
+#include <sys/wait.h>  // waitpid()
+#include <sys/types.h> // pid type
+#include <time.h>      // time pour rand()
+#include <sys/shm.h>   // Pour la zone de memoire partagee
+#include <sys/sem.h>   // pour utiliser les semaphores
+#include <signal.h>    // Pour envoyer des signaux
 
 #define ATTENTE 0
 #define TRAVAILLE 1
@@ -21,6 +14,70 @@
 int sem_set;
 char *etat_mineur;
 int mineurs, pioches, pelles;
+
+void sem_init(int sem, int val);
+void sem_op(int sem, int sem_op);
+void sem_wait(int sem);
+void sem_signal(int sem);
+_Bool travail_mineur(int m);
+void commence_travail(int mineur_id);
+void attente_travail(int mineur_id);
+void stop_travail(int mineur_id);
+void fils(int mineur_id);
+
+int main()
+{
+    printf("Combien de mineurs , pioches et pelles ?\n");
+    scanf("%d %d %d", &mineurs, &pioches, &pelles);
+
+    printf("---   %d Mineurs | %d Pioches | %d Pelles   ---\n", mineurs, pioches, pelles);
+
+    // Création Memoire Partagée
+    int shmid = shmget(IPC_PRIVATE, mineurs, IPC_CREAT | 0660);
+    etat_mineur = (char *)shmat(shmid, NULL, 0);
+
+    int pid;
+
+    // Creation des Sémaphores, le dernier est le Mutex
+    sem_set = semget(IPC_PRIVATE, mineurs + 1, IPC_CREAT | 0660);
+    if (sem_set == -1)
+    {
+        printf("Erreur de mémoire partagée.\n");
+        exit(1);
+    }
+
+    // On initialise tout les semaphores
+    int i;
+    for (i = 0; i < mineurs; i++)
+        sem_init(i, 0); // On initialise les semaphores a 0
+    sem_init(i, 1);
+
+    // A la creation tout les mineurs attendent pour travailler
+    for (i = 0; i < mineurs; i++)
+        etat_mineur[i] = ATTENTE;
+
+    // Création des différents mineurs
+    int num_processus;
+    for (num_processus = 1; num_processus <= mineurs; num_processus++)
+    {
+        pid = fork();
+        if (pid == 0)
+            break;
+    }
+
+    if (pid == 0)
+        fils(num_processus);
+    else if (pid < 0)
+    {
+        printf("Erreur de fork\n");
+        exit(1);
+    }
+    else
+        for (int i = 1; i <= mineurs; i++)
+            waitpid(-1, NULL, 0); // On attend que tout les processus fils se terminent
+
+    return 0;
+}
 
 // Initialisation d'un Sémaphore.
 void sem_init(int sem, int val)
@@ -65,12 +122,12 @@ void commence_travail(int mineur_id)
     sem_signal(mineur_id);
     pioches--;
     pelles--;
+    printf("Mineur %d > Prends 1 Pelle et 1 Pioche (Reste %d pelles et %d pioches).\n", mineur_id, pelles, pioches);
 }
 
 void attente_travail(int mineur_id)
 {
     sem_wait(mineurs);
-    printf("Le mineur %d prend une pelle et une pioche.\n", mineur_id);
     etat_mineur[mineur_id] = ATTENTE;
     if (travail_mineur(mineur_id))
         commence_travail(mineur_id);
@@ -92,6 +149,8 @@ void fils(int mineur_id)
 {
     srand(time(NULL) ^ (getpid() << 16));
 
+    printf("\n---\n| Creation du mineur %d (%d)\n---\n\n", mineur_id, getpid());
+
     for (int i = 1; i < mineurs; i++)
     {
         int r, r2, or, h;
@@ -99,102 +158,19 @@ void fils(int mineur_id)
         r = rand() % (5 - 1) + 1;
         r2 = rand() % (5 - 1) + 1;
 
-        h = 0;
-
-        for (int j = 0; j < r; j++)
-        {
-            sleep(1);
-            printf(
-                "Mineur %d | h%d > Attends avant de travailler (encore %dh).\n",
-                mineur_id,
-                h,
-                r - j);
-            h++;
-        }
+        printf("Mineur %d > Attends %dh avant de travailler.\n", mineur_id, r);
+        sleep(r);
         attente_travail(mineur_id);
 
-        for (int j = 0; j < r2; j++)
-        {
-            sleep(1);
-            printf(
-                "Mineur %d | h%d > Travaille (encore %dh).\n",
-                mineur_id,
-                h,
-                r2 - j);
-            h++;
-        }
+        printf(
+            "Mineur %d > Travaille (encore %dh).\n", mineur_id, r2);
+        sleep(r);
 
         printf(
-            "\n--- Mineur %d | h%d > %dg d'Or Extrait ---\n\n",
+            "\n---   Mineur %d > %dg d'Or Extrait   ---\n\n",
             mineur_id,
-            h,
             or);
-
-        // printf("Le mineur %d rend une pelle et une pioche\n", m);
-        // printf("Le Mineur %d a fini de travailler! (%d)\n", m, getpid());
 
         stop_travail(mineur_id);
     }
-}
-
-int main()
-{
-    printf("Combien de mineurs , pioches et pelles ?\n");
-    scanf("%d %d %d", &mineurs, &pioches, &pelles);
-
-    printf("# %d Mineurs | %d Pioches | %d Pelles\n", mineurs, pioches, pelles);
-
-    int shmid;
-    shmid = shmget(IPC_PRIVATE, mineurs, IPC_CREAT | 0660); // Creation memoire partagee
-    etat_mineur = (char *)shmat(shmid, NULL, 0);
-
-    int pid;
-
-    // Creation des semaphores, le dernier est le mutex
-    sem_set = semget(IPC_PRIVATE, mineurs + 1, IPC_CREAT | 0660);
-    if (sem_set == -1)
-    {
-        printf("Erreur de memoire partagee\n");
-        exit(1);
-    }
-
-    // On initialise tout les semaphores
-    int i;
-    for (i = 0; i < mineurs; i++)
-        sem_init(i, 0); // On initialise les semaphores a 0
-    // printf("%d", i);
-    sem_init(i, 1);
-
-    // A la creation tout les mineurs attendent pour travailler
-    for (i = 0; i < mineurs; i++)
-        etat_mineur[i] = ATTENTE;
-
-    // On cree les differents mineurs
-    int num_processus;
-    for (num_processus = 1; num_processus <= mineurs; num_processus++)
-    {
-        pid = fork();
-        if (pid == 0)
-            break;
-    }
-
-    if (pid == 0)
-    {
-        printf("Creation du mineur %d (%d)\n", num_processus, getpid());
-        fils(num_processus);
-    }
-    else if (pid < 0)
-    {
-        printf("Erreur de fork\n");
-        exit(1);
-    }
-    else
-    {
-        for (int i = 1; i <= mineurs; i++)
-        {
-            waitpid(-1, NULL, 0); // On attend que tout les processus fils se terminent
-        }
-    }
-
-    return 0;
 }
